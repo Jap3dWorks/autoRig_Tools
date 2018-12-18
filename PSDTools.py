2de7f348156b6e456e71bb92a35b32bdf81e7438
Jordi <Jap3dWorks@gmail.com>
2018-12-19 00:11:58 +0100

    Add getdeltaByJointAngle, need more work, error getting quaternion rotation. (joint orient)

--- PSDTools.py
+++ PSDTools.py
@@ -1,6 +1,7 @@
 from maya import cmds
 import pymel.core as pm
 from maya import OpenMaya
+import math
 
 
 def extractPosesPoseEditor():
@@ -117,81 +118,168 @@ def getDelta(positive, negative, base):
 
     negBiNorVec = OpenMaya.MVectorArray()
     baseBiNorVec = OpenMaya.MVectorArray()
-    tangentIndex = []
 
-    # get Tangent component (position of the lower index connected vertex)
+    # get Tangents
     for i in diferenceIndex:
+        floatVector = OpenMaya.MVector()
+        floatBiNormal = OpenMaya.MVector()
+        baseVector = OpenMaya.MVector()
+        baseBiNormal = OpenMaya.MVector()
+
         ptr = util.asIntPtr()
         negativeIt.setIndex(i, ptr)
-        connectedVertex = OpenMaya.MIntArray()
-        negativeIt.getConnectedVertices(connectedVertex)
-        tangentIndex.append(min(connectedVertex))  # lower index vertex
+        faces = OpenMaya.MIntArray()
+        negativeIt.getConnectedFaces(faces)
+
+        negativeMFN.getFaceVertexTangent(faces[0], i, floatVector)
+        negativeMFN.getFaceVertexBinormal(faces[0], i, floatBiNormal)
+
+        baseMFN.getFaceVertexTangent(faces[0], i, baseVector)
+        baseMFN.getFaceVertexBinormal(faces[0], i, baseBiNormal)
+
+        negativeTVec.append(floatVector)
+        negBiNorVec.append(floatBiNormal)
+        baseTVec.append(baseVector)
+        baseBiNorVec.append(baseBiNormal)
 
     # apply martix transforms
     for n, i in enumerate(diferenceIndex):
         # negative
         normal = OpenMaya.MVector()
         negativeMFN.getVertexNormal(i, normal)
-        vertexPosition = OpenMaya.MPoint()
-        negativeMFN.getPoint(i, vertexPosition, OpenMaya.MSpace.kObject)
-
-        # calculate tangent and binormal, temporal
-        tangent = OpenMaya.MPoint()
-        negativeMFN.getPoint(tangentIndex[n], tangent, OpenMaya.MSpace.kObject)
-        tangent = OpenMaya.MVector(tangent - vertexPosition)
-        tangent.normalize()
-        # binormal
-        binormal = normal ^ tangent
+        binormal = negBiNorVec[n]
         binormal.normalize()
-        # tangent corrected
-        tangent = binormal ^ normal
+        tangent = negativeTVec[n]
         tangent.normalize()
-        # matrix
         matrixSpaceNegative = [normal.x, normal.y, normal.z, 0, tangent.x, tangent.y, tangent.z, 0, binormal.x,
                                binormal.y, binormal.z, 0, 0, 0, 0, 1]
         matrixNeg = OpenMaya.MMatrix()
         util.createMatrixFromList(matrixSpaceNegative, matrixNeg)
 
+        matrixNeg3x3 = pm.datatypes.MatrixN([[normal.x, normal.y, normal.z], [tangent.x, tangent.y, tangent.z],
+                                             [binormal.x, binormal.y, binormal.z]])
 
         # base
-        # negative
         normal = OpenMaya.MVector()
         baseMFN.getVertexNormal(i, normal)
-        vertexPosition = OpenMaya.MPoint()
-        baseMFN.getPoint(i, vertexPosition, OpenMaya.MSpace.kObject)
-
-        # calculate tangent and binormal, temporal
-        tangent = OpenMaya.MPoint()
-        baseMFN.getPoint(tangentIndex[n], tangent, OpenMaya.MSpace.kObject)
-        tangent = OpenMaya.MVector(tangent - vertexPosition)
-        tangent.normalize()
-        # binormal
-        binormal = normal ^ tangent
+        binormal = baseBiNorVec[n]
         binormal.normalize()
-        # tangent corrected
-        tangent = binormal ^ normal
+        tangent = baseTVec[n]
         tangent.normalize()
         matrixSpaceBase = [normal.x, normal.y, normal.z, 0, tangent.x, tangent.y, tangent.z, 0, binormal.x, binormal.y,
                            binormal.z, 0, 0, 0, 0, 1]
         matrixBas = OpenMaya.MMatrix()
         util.createMatrixFromList(matrixSpaceBase, matrixBas)
+        matrixBas3x3 = pm.datatypes.MatrixN([[normal.x, normal.y, normal.z], [tangent.x, tangent.y, tangent.z],
+                                             [binormal.x, binormal.y, binormal.z]])
+
 
         # diferenceVector
         vectorPosed = positive.getPoint(i) - negative.getPoint(i)
-        print vectorPosed
         vectorPosed = OpenMaya.MVector(vectorPosed[0], vectorPosed[1], vectorPosed[2])
+        vectorPosedPM = pm.datatypes.MatrixN([vectorPosed[0], vectorPosed[1], vectorPosed[2]])
 
-        vecNegSpace = vectorPosed * matrixNeg.inverse()
+        # TODO: calculate real vector length
+        # cmds.skinPercent( 'skinCluster1', 'akona_body_mesh.vtx[2702]', transform='akona_foot_left_joint', query=True )
+
         # baseSpace
-        vecBaseSpace = vecNegSpace * matrixBas
+        vecNegSpace = vectorPosedPM * matrixNeg3x3.inverse()
+        vecBaseSpace = vecNegSpace * matrixBas3x3
+        # compare vector length form joint position
+
 
         # apply diference
         originalPos = base.getPoint(i, 'object')
-        VertexPos = [originalPos[0] + vecBaseSpace.x, originalPos[1] + vecBaseSpace.y, originalPos[2] + vecBaseSpace.z]
+
+        VertexPos = [originalPos[0] + vecBaseSpace[0][0], originalPos[1] + vecBaseSpace[0][1], originalPos[2] + vecBaseSpace[0][2]]
         baseDup.setPoint(i, VertexPos, 'object')
 
     baseDup.getTransform().rename('delta')
     return baseDup
+
+
+def getDeltaByJointAngle(positive, negative, skinMesh,  joint):
+    """
+    only one rotated joint
+    test, i think it is more precise
+    :return:
+    """
+    diferenceIndex = []
+    for i, point in enumerate(positive.getPoints('object')):
+        if point != negative.getPoint(i, 'object'):
+            diferenceIndex.append(i)
+
+    skinCluster = skinMesh.listConnections(connections=True, type='skinCluster')[0][1]
+    skinCluster = pm.PyNode(skinCluster)  # convert to pyNode
+
+    # query angles
+    angleQ = joint.getRotation(quaternion=True, space='preTransform')
+    worldQ = joint.getRotation(quaternion=True, space='world')
+    angleEu = joint.getRotation()  # euler
+    angle = math.acos(angleQ[3])*2  # angle Radians
+    joint.setRotation([0, 0, 0])
+    jointZeroR = joint.getRotation(quaternion=True, space='world')
+
+    #angleQ = angleQ * jointZeroR.invertIt()
+
+    jointMatrix = pm.xform(joint, q=True, m=True, ws=True)
+    jointMatrix = pm.datatypes.MatrixN([jointMatrix[0], jointMatrix[1], jointMatrix[2]],
+                                       [jointMatrix[4], jointMatrix[5], jointMatrix[6]],
+                                       [jointMatrix[8], jointMatrix[9], jointMatrix[10]])
+
+    jointOrient = joint.getOrientation()
+
+    # joint position
+    jointPos = joint.getTranslation('world')
+
+    # create base pose
+    baseMesh = skinMesh.duplicate(name='%s_delta' % str(skinMesh))[0]
+    baseMeshShape = baseMesh.getShape()
+    joint.setRotation(angleEu)
+
+    for index in diferenceIndex:
+        influence = pm.skinPercent(skinCluster, skinMesh.vtx[index], transform=joint, query=True)
+        # influence plus influences of child joints
+        joinChild = [str(j) for j in joint.listRelatives(ad=True)]
+        jointsInfluence = pm.skinPercent(skinCluster, skinMesh.vtx[index], q=True, transform=None)
+        matchJoint = set(joinChild).intersection(set(jointsInfluence))
+        for j in matchJoint:
+            influence += pm.skinPercent(skinCluster, skinMesh.vtx[index], transform=j, query=True)
+
+        # vector from joint to vertex sculpted
+        sculptVector = positive.getPoint(index, 'object')
+        sculptVector = pm.datatypes.Vector(sculptVector - jointPos)
+
+        # if influence == 0 don't calculate
+        if influence:
+            angleA = math.pi - (angle/2 + math.pi/2)
+            angleB = math.pi - (angle*influence + angleA)
+            # with sin rules get the length of final vector
+            lengthFVector = sculptVector.length()*math.sin(angleB)/math.sin(angleA)
+
+            # create relative quaternion to influence
+            qW = math.cos(angle*(-influence) / 2)
+            util = OpenMaya.MScriptUtil()
+            util.createFromDouble(angleQ.x, angleQ.y, angleQ.z, qW)
+            ptr = util.asDoublePtr()
+            relativeQ = pm.datatypes.Quaternion(ptr)
+
+            rotatedVector = sculptVector.rotateBy(relativeQ)
+            rotatedVector.normalize()
+            rotatedVector = rotatedVector*lengthFVector
+
+            sculptVector = rotatedVector
+
+        baseMeshShape.setPoint(index, sculptVector + jointPos, 'world')
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
