08164b4b6fc56a2fd34e14349c125c6a654e9d29
Jordi <Jap3dWorks@gmail.com>
2019-01-02 13:35:22 +0100

    Change akonaRigA func
    more brief

--- akona_AutoRig.py
+++ akona_AutoRig.py
@@ -4,6 +4,7 @@ import pymel.core as pm
 import re
 
 import autoRig
+reload(autoRig)  # review: delete
 
 def import_model(path='D:/_docs/_Animum/Akona/skinCluster/akona_skinPSD.ma'):
     cmds.file(new=True, force=True)
@@ -13,7 +14,6 @@ def import_model(path='D:/_docs/_Animum/Akona/skinCluster/akona_skinPSD.ma'):
 
 def akonaRigA(name='akona', path='D:\_docs\_Animum\Akona'):
     # spine Head
-    reload(autoRig)  # review: delete
     akonaRig = autoRig.RigAuto(chName=name, path=path)  # create object
     # spine
     akonaRig.spine_auto('spine', lambda: akonaRig.addCluster('akona_chest_cluster', akonaRig.spineIKControllerList[-2], 'chest_cluster'),
@@ -21,19 +21,15 @@ def akonaRigA(name='akona', path='D:\_docs\_Animum\Akona'):
     # neckHead
     akonaRig.neckHead_auto()
 
+    sides = ['left', 'right']  # side types
     # legs
-    akonaRig.ikFkChain_auto('left', akonaRig.ikControllers['spine'][0], 'leg', True, True,
-                            lambda: akonaRig.foot_auto('left', ('leg', 'foot', 'toe'), 'zx'))
-    akonaRig.ikFkChain_auto('right', akonaRig.ikControllers['spine'][0], 'leg', True, True,
-                            lambda: akonaRig.foot_auto('right', ('leg', 'foot', 'toe'), 'zx'))
+    for side in sides:
+        akonaRig.ikFkChain_auto(side, akonaRig.ikControllers['spine'][0], 'leg', True, True,
+                                lambda: akonaRig.foot_auto(('leg', 'foot', 'toe'), 'zx'))
 
     # arms
-    akonaRig.ikFkChain_auto('left', akonaRig.ikControllers['spine'][-1], 'arm', True, False,
-                            lambda: akonaRig.hand_auto('left', ('arm', 'hand', 'finger'), None),
-                            lambda: akonaRig.clavicle_auto('left', 'clavicle'),
-                            lambda: akonaRig.ikFkChain_wire('akona_body_mesh', 'NONE'))
-
-    akonaRig.ikFkChain_auto('right', akonaRig.ikControllers['spine'][-1], 'arm', True, False,
-                            lambda: akonaRig.hand_auto('right', ('arm', 'hand', 'finger'), None),
-                            lambda: akonaRig.clavicle_auto('right', 'clavicle'),
-                            lambda: akonaRig.ikFkChain_wire('akona_body_mesh', 'NONE'))
+    for side in sides:
+        akonaRig.ikFkChain_auto(side, akonaRig.ikControllers['spine'][-1], 'arm', True, False,
+                                lambda: akonaRig.hand_auto(('arm', 'hand', 'finger'), None),
+                                lambda: akonaRig.clavicle_auto('clavicle'),
+                                lambda: akonaRig.ikFkChain_wire('akona_body_mesh', 'pole'))
